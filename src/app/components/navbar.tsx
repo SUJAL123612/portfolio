85140107b596e3d485e4105eab68149a0af46dc9
SuJaL3511 <95642419+SUJAL123612@users.noreply.github.com>
2025-07-16 21:34:04 +0530

    fix

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,15 +1,16 @@
+import Link from "next/link";
+
 export default function Navbar() {
-    return (
-      <>
-      <div className="nav_sec">
+  return (
+    <>
+      <nav>
         <ul className="navbar">
-            <li className="nav_name">Home</li>
-            <li className="nav_name">About Us</li>
-            <li className="nav_name">Projects</li>
-            <li className="nav_name">Contacts</li>
+          <Link href="/home">Home</Link>
+          <Link href="/about">About Us</Link>
+          <Link href="/projects">Projects</Link>
+          <Link href="/contacts">Contacts</Link>
         </ul>
-      </div>
-      </>
-    );
-  }
-  
+      </nav>
+    </>
+  );
+}
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,9 +4,9 @@ export default function Projects() {
     return (
         <>
             <Navbar />
-            <div className="projects_sec">
-                <h2 className="section-title">JS Projects</h2>
-                <ol className="alpha-list">
+            <div className="m-[40px auto] p-[20px] bg-white-500 ">
+                <h2 className=" text-black-600/100 text-center font-weight: 700; text-2xl inset-x-0 bottom-0 h-16">JS Projects</h2>
+                <ol className="text-black-300/100 list-[lower-alpha] text-lg ps-8">
                     <li>Calculator</li>
                     <li>Tic-Tac-Toe</li>
                     <li>Expense Tracker</li>
