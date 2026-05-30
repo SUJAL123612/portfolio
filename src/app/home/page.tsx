6f146a4021e362c14f5c51bece11d33db4d3341b
SuJaL3511 <95642419+SUJAL123612@users.noreply.github.com>
2026-05-30 14:58:18 +0530

    commit

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
           <p className="text-[20px] rounded-full font-sans text-gray-300 ml-[100px] mt-[20px] cursor-pointer bg-purple-500 hover:bg-purple-600 hover:text-white w-60 pr-5 pl-5 pt-2 pb-2">DOWNLOAD RESUME</p>
         </div>
         {/* <div className="w-70 h-70 rounded-full overflow-hidden border-4 border-purple-600 mt-[50px] ml-[180px]"> */}
-          <img src="profile.jpg" alt="profile" className="w-[550px] h-[450px] object-contain mt-[10px]"/>
+          <img src="profil.jpg" alt="profile" className="w-[550px] h-[450px] object-contain mt-[10px]"/>
         {/* </div> */}
       </div>
     </>
